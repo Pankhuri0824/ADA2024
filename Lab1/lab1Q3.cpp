5e691ba06cd16f3a756070129ea83937a7c08370
Pankhuri Singh <panlhuri22348@iiitd.ac.in>
2024-02-04 04:00:59 +0530

    cs not for me - side note Q3,4 done

--- Lab1/lab1Q3.cpp
+++ Lab1/lab1Q3.cpp
@@ -8,7 +8,43 @@
 #include <climits>
  
 using namespace std;
- 
+ //static_cast<char>('a' + 1)
+
+int recursion(int n,string s,char c){
+
+    //base
+    if(n==1){
+        if(s[0]==c) return 0;
+        else return 1;
+    }
+
+    int ind=n/2;
+
+    int ops=0;
+    int ops2=0;
+
+    string one=s.substr(0,ind);;
+    string two=s.substr(ind,ind);
+
+    for(int i=0;i<ind;i++){
+        if(c!=s[i]){
+            ops++;
+        }
+    }
+    for(int i=ind;i<n;i++){
+        if(c!=s[i]){
+            ops2++;
+        }
+    }
+
+    int oneans=ops2+recursion(n/2,one,(c + 1));
+    int twoans=ops+recursion(n/2,two,(c + 1));
+
+    return min(oneans,twoans);
+
+}
+
+
 int main(){
     int t;// test cases
     cin>>t;
@@ -17,9 +53,10 @@ int main(){
         cin>>n; //lenth of string & n=2^k k>=0
         string s;
         cin>>s; //req string
-
-        
+        int a=recursion(n,s,'a');
+        cout<<a<<endl;
     }
+
     return 0;
  
 }
--- Lab1/lab1Q4.cpp
+++ Lab1/lab1Q4.cpp
@@ -8,95 +8,64 @@
 #include <climits>
  
 using namespace std;
- 
-bool recursion(long long int left,long long int right,vector<long long int>& array,long long int stat,vector<long long int>& temp){
-    if((right==left)){
-        return (array[left]==stat);
+
+void recursion1(int left,int right,vector<int>& array,set<int>& s1){
+    if(left==right){
+        return;
     }
 
-    // use median and split array into 2
-    long long int med=(array[left]+array[right])/2; 
-    //1 2 3 4 5 -> 3 -> 123 , 45-> we need to divide by med ele ind+1 = something just bigger than that which is upper bound
-    auto medd= upper_bound(array.begin() + left, array.begin() + right + 1,med);
-    long long int medit =distance(array.begin(), medd);
-    long long int lsum=0;
-    if (left!=0){
-        lsum=temp[medit-1]-temp[left-1];
-    }
-    else{ lsum=temp[medit-1];}
-    long long int rsum=temp[right]-temp[medit-1];
- 
-    
-    if(lsum==stat || rsum==stat || rsum+lsum==stat){
-        return true;
-    }
-    else if(array[left]==array[right]){
-        return false;
-    }
-    else{
-        if(stat<=med && lsum>=stat){
-            return recursion(left,medit -1, array, stat,temp);
-        }
-        else if(rsum>=stat){
-            bool check= recursion(medit,right, array, stat,temp);
-            if(check==true){
-                return true;
-            }
-            else{
-                if(lsum>=stat){
-                  return recursion(left,medit -1, array, stat,temp);
-                }
-                else{
-                    return false;
-                }
-            }
+    int med=(array[left]+array[right])/2;
+
+    int lsum=0;
+    int rsum=0;
+    int ind=left;
+
+    for(int j=left;j<=right;j++){
+        if(array[j]<=med){
+            lsum+=(array[j]);
+            ind++;
         }
         else{
-            return false;
+            rsum+=(array[j]);
         }
     }
- 
+
+    s1.insert(lsum);
+    s1.insert(rsum);
+    s1.insert(lsum+rsum);
+
+    if(array[left]==array[right]){
+        return;
+    }
+    recursion1(left,ind-1,array,s1);
+    recursion1(ind,right,array,s1);
 }
  
 int main(){
     long long int t;// test cases
     cin>>t;
     for(int test=0;test<t;test++){
-        long long int n,q;
+        int n,q;
         cin>>n>>q; //n=no of pokemons, q=lenth of oak's array
  
-        vector<long long int> arr(n);//friendliness stat of n pokemons
+        vector<int> arr(n);//friendliness stat of n pokemons
         for(int i=0;i<n;i++){
             cin>>arr[i];
         }
  
         sort(arr.begin(),arr.end());
- 
-        vector<long long int> add(n);
-        long long int temp=0;
-        for(int i=0;i<n;i++){
-            temp+=arr[i];
-            add[i]=temp;
-        }
+        set<int> possible;
+        recursion1(0,n-1,arr,possible);
+
  
         for(int i=0;i<q;i++){ //q=number of queries
-            long long int a;
+            int a;
             cin>>a;
-            if(n==1){
-                if(a==arr[0]){
-                    cout<<"YES"<<endl;
-                }
-                else{
-                    cout<<"NO"<<endl;
-                }
+            if(possible.find(a)!=possible.end()){
+                cout<<"Yes"<<endl;
             }
             else{
-                if(recursion(0,n-1, arr, a,add)){
-                cout<<"Yes"<<endl;
-                }
-                else{
-                    cout<<"No"<<endl;
-                }
+                cout<<"No"<<endl;
             }
         }
  
