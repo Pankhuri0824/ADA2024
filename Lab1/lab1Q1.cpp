80b78fca7224284d52ce3ee7746d9e9fd8eb7852
Pankhuri Singh <panlhuri22348@iiitd.ac.in>
2024-02-04 15:01:22 +0530

    Q1 killed the dawg in me - tc error on 6

--- Lab1/lab1Q1.cpp
+++ Lab1/lab1Q1.cpp
@@ -10,36 +10,42 @@
 using namespace std;
 
 bool recursion(vector<int> &arr, int index, vector<int> &subarr, int n, int d){
-    if (index==n) {//idk but works?? 
-        int sum =0;
-        for(int j=0; j<n; j++){
-            sum += subarr[j];
-        }
-
-        if(sum % d == 0){
-            return true;
+        // cout<<index<<endl;
+        if (index==n) {//idk but works??  
+            if(subarr.size()!=0){
+                int sum =0;
+                for(int j=0; j<n; j++){
+                    sum += subarr[j];
+                // cout<<subarr[j]<<" ";
+                }
+                // cout<<" "<<endl;
+                if(sum % d == 0){
+                    // cout<<"true"<<endl;
+                    return true;
+                }
+                else{
+                    return false;
+                }
+            }
         }
         else{
-            return false;
+            subarr.push_back(arr[index]); //pick
+            if(recursion(arr, index + 1, subarr, n, d)){
+                return true; // true from recursion hence truw for all
+            }
+            subarr.pop_back(); 
+
+            if(recursion(arr, index + 1, subarr, n, d)){ //not pick
+                return true; 
+            }
+            else{
+                // cout<<"false"<<endl;
+                return false;
+            }
         }
+
+        return false; //no cond met
     }
-    else{
-        subarr.push_back(arr[index]); //pick
-        if(recursion(arr, index + 1, subarr, n, d)){
-            return true; // true from recursion hence truw for all
-        }
-        subarr.pop_back(); 
-
-
-        
-
-        if(recursion(arr, index + 1, subarr, n, d)){ //not pick
-            return true; 
-        }
-    }
-
-    return false; //no cond met
-}
 
 int main()
 {
@@ -58,11 +64,27 @@ int main()
             total+=nums[i];
         }
 
-        if(n==1){
+        if(n==1 || n==0){
             cout<<"NO"<<endl;
         }
+        if(n==2){
+            bool c=true;
+            if(nums[0]%d!=0) c=false;
+            else if(nums[1]%d!=0) c=false;
+
+            if(c) cout<<"YES"<<endl;
+            else cout<<"NO"<<endl;
+        }
         else{
-            bool check = recursion(nums,0,subnums,n,d);
+
+            bool check=true;
+            for(int i=0;i<n;i++){
+                int remind=i; //remove index NOT REMIND
+                vector<int> checker=nums;
+                checker.erase(checker.begin()+remind);
+                check = check||recursion(checker,0,subnums,n-1,d);
+                if(check==false) break;
+            }
             if(check){
                 cout<<"YES"<<endl;
             }
