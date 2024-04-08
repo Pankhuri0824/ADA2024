ed32b6357461030e78a6133210189958fa746f10
Pankhuri0824 <pankhuri22348@iiitd.ac.in>
2024-04-08 15:28:22 +0530

    "Lab2 Q2 done"

---
+++ Lab2/Lab2Q2.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <set>
+#include<vector>
+#include <map>
+ 
+using namespace std;
+
+int main(){
+    int q;
+    cin>>q;
+    vector<vector<int>> v;// result ors storing ds
+
+    vector<int> row(1024,0);
+    row[0]=1; //initially 0 is there bc 0 ways to rEACH 0
+    v.push_back(row);
+
+
+    for(int t=0;t<q;t++){
+        int mode;
+        int num;
+        cin>>mode>>num;
+
+        int n= v.size()-1;
+        // cout<<"size: "<<n<<endl;
+        if(mode==1){
+            
+            vector<int> row ;
+            row.assign(v[n].begin(), v[n].end());
+
+            for(int i=0;i<10;i++){
+                if(v[n][i]>0){
+                    int res = i | num ;
+                    row[res]=(row[res] + v[n][i])% 1000000007;
+                }
+            }
+
+            v.push_back(row);
+            for(int i=0;i<16;i++){
+                cout<<v[n+1][i]<<" ";
+            }
+            cout<<endl;
+        }
+
+        else if(mode==2){
+
+            cout<<v[n][num]<<endl;
+        }
+
+    }
+
+    
+}
+
+
+// TC : 
+// 6
+// 2 0
+// 2 1
+// 1 2
+// 1 2
+// 2 2
+// 2 1000
---
+++ Lab2/README.md
@@ -0,0 +1,7 @@
+
+Lab 1 Questions :
+- Party Time :**
+- The teleportation Machine : **
+- Artist's Dilemma : **
+- The Cooper-Hofstadler Realization : **
+- RIISE ONCE AGAIN : ** 
